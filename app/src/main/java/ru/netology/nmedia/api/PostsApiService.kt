6db71dd0af18c4383a6be800bc96ad1fb040ab74
Vlad Fisenko <Vladf.fisenko.007@gmail.com>
2025-09-05 19:40:49 +0600

    add function Like and Remove

--- app/src/main/java/ru/netology/nmedia/api/PostsApiService.kt
+++ app/src/main/java/ru/netology/nmedia/api/PostsApiService.kt
@@ -5,7 +5,11 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import ru.netology.nmedia.BuildConfig
 import ru.netology.nmedia.dto.Post
 
